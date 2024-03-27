92ba47b8cced415bc273bca1bcee0f5c3a722222
Mordechai Meisels <mordechai@codaworks.com>
2024-03-27 14:52:16 -0400

    Add react-email with Resend

---
+++ src/emails/layout.js
@@ -0,0 +1,20 @@
+import { Font, Head, Html, Tailwind } from '@react-email/components'
+import config from '../../tailwind.config'
+
+export const EmailLayout = ({ children }) => {
+    return <Html>
+        <Head>
+            <Font
+                fontFamily='Inter'
+                fallbackFontFamily='Helvetica'
+                webFont={{
+                    url: 'https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7.woff2',
+                    format: 'woff2'
+                }}
+            />
+        </Head>
+        <Tailwind config={config}>
+            {children}
+        </Tailwind>
+    </Html>
+}
